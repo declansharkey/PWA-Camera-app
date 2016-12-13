13d61b9616cfb4245cfaae5ff62c507dff7be962
Declan Sharkey <dsharkey@pnimedia.com>
2016-12-12 17:03:59 -0800

    Centre alignment and some small formatting changes

--- PWA-Camera-app/scripts/camera.js
+++ PWA-Camera-app/scripts/camera.js
@@ -31,17 +31,14 @@ document.addEventListener('DOMContentLoaded', function() {
             },
             // Success Callback
             function(stream) {
-
                 // Create an object URL for the video stream and
                 // set it as src of our HTLM video element.
                 video.src = window.URL.createObjectURL(stream);
-
                 // Play the video element to start the stream.
                 video.play();
                 video.onplay = function() {
                     showVideo();
                 };
-
             },
             // Error Callback
             function(err) {
@@ -55,33 +52,24 @@ document.addEventListener('DOMContentLoaded', function() {
     // Mobile browsers cannot play video without user input,
     // so here we're using a button to start it manually.
     start_camera.addEventListener("click", function(e) {
-
         e.preventDefault();
-
         // Start video playback manually.
         video.play();
         showVideo();
-
     });
 
 
     take_photo_btn.addEventListener("click", function(e) {
-
         e.preventDefault();
-
         var snap = takeSnapshot();
-
         // Show image. 
         image.setAttribute('src', snap);
         image.classList.add("visible");
-
         // Enable delete and save buttons
         delete_photo_btn.classList.remove("disabled");
         download_photo_btn.classList.remove("disabled");
-
         // Set the href attribute of the download button to the snap url.
         download_photo_btn.href = snap;
-
         // Pause video playback of stream.
         video.pause();
 
@@ -89,61 +77,44 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     delete_photo_btn.addEventListener("click", function(e) {
-
         e.preventDefault();
-
         // Hide image.
         image.setAttribute('src', "");
         image.classList.remove("visible");
-
         // Disable delete and save buttons
         delete_photo_btn.classList.add("disabled");
         download_photo_btn.classList.add("disabled");
-
         // Resume playback of stream.
         video.play();
-
     });
 
     delete_photo_btn.addEventListener("click", function(e) {
-
         e.preventDefault();
-
         // Hide image.
         image.setAttribute('src', "");
         image.classList.remove("visible");
-
         // Disable delete and save buttons
         delete_photo_btn.classList.add("disabled");
         download_photo_btn.classList.add("disabled");
-
         // Resume playback of stream.
         video.play();
-
     });
 
 
 
     function takeSnapshot() {
         // Here we're using a trick that involves a hidden canvas element.  
-
         var hidden_canvas = document.querySelector('canvas'),
             context = hidden_canvas.getContext('2d');
-
         var width = video.videoWidth,
             height = video.videoHeight;
-
         if (width && height) {
-
             // Setup a canvas with the same dimensions as the video.
             hidden_canvas.width = width;
             hidden_canvas.height = height;
-
             // Make a copy of the current frame in the video on the canvas.
             context.drawImage(video, 0, 0, width, height);
-
             // Turn the canvas image into a dataURL that can be used as a src for our photo.
-
         }
     }
 
@@ -151,11 +122,9 @@ document.addEventListener('DOMContentLoaded', function() {
     function mirrorImage() {
         var hidden_canvas = document.querySelector("camera-stream");
         var context = hidden_canvas.getContext("2d");
-
         // flip context horizontally
         context.scale(-1, 1);
         console.log(context);
-
         //check to see if the function is firing
         console.log('Button clicked');
     }
@@ -173,9 +142,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (error) {
             console.error(error);
         }
-
         error_message.innerText = error_msg;
-
         hideUI();
         error_message.classList.add("visible");
     }
@@ -183,12 +150,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function hideUI() {
         // Helper function for clearing the app UI.
-
         controls.classList.remove("visible");
         start_camera.classList.remove("visible");
         video.classList.remove("visible");
         snap.classList.remove("visible");
         error_message.classList.remove("visible");
     }
-
 });
